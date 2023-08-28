24cf35f5cb1bf9da9fd1f9f943eb2fbc000409b6
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-08-28 22:57:15 +0900

    added filter word at review admin page

--- reviews/admin.py
+++ reviews/admin.py
@@ -2,6 +2,26 @@ from django.contrib import admin
 from .models import Review
 
 
+class WordFilter(admin.SimpleListFilter):
+    title = "Filter by word"
+
+    parameter_name = "filter by word"
+
+    def lookups(self, request, model_admin):
+        return [
+            ("good", "Good"),
+            ("greate", "Greate"),
+            ("awesome", "Awesome"),
+        ]
+
+    def queryset(self, request, queryset):
+        # print(queryset[2].review)
+        keyword = self.value()
+        if keyword == None:
+            return queryset
+        return queryset.filter(review__contains=keyword)
+
+
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
     list_display = (
@@ -9,4 +29,9 @@ class ReviewAdmin(admin.ModelAdmin):
         "review",
     )
 
-    list_filter = ("rating",)
+    list_filter = (
+        WordFilter,
+        "rating",
+        "user__is_host",
+        "room__category",
+    )
