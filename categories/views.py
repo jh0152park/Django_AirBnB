b57481e2e04896f885431162567b0e41c4f0ec09
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-08-29 21:46:53 +0900

    created django rest api

--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Category
+
 
 # Create your views here.
+def all_categories(request):
+    all_categories = Category.objects.all()
+    return JsonResponse(
+        {
+            "status": "success",
+        },
+    )
