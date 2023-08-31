878399ad1a5d16da793b7abbf560140ecb05ffb9
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-08-31 16:53:06 +0900

    created amenity detail functions

--- rooms/views.py
+++ rooms/views.py
@@ -2,20 +2,21 @@
 # from django.http import HttpResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
+from rest_framework.exceptions import NotFound
+from rest_framework.status import HTTP_204_NO_CONTENT
 from .models import Room
 from .models import Amenity
 from .serializers import AmenitySerializer
 
 
 class Amenities(APIView):
-    def get(self, requset):
+    def get(self, request):
         all_amenities = Amenity.objects.all()
         serializer = AmenitySerializer(all_amenities, many=True)
         return Response(serializer.data)
 
-    def post(self, requset):
-        serializer = AmenitySerializer(data=requset.data)
+    def post(self, request):
+        serializer = AmenitySerializer(data=request.data)
         if serializer.is_valid():
             amenity = serializer.save()
             return Response(
@@ -26,14 +27,40 @@ class Amenities(APIView):
 
 
 class AmenityDetail(APIView):
-    def get(self, requset, pk):
-        pass
+    def get_object(self, pk):
+        try:
+            return Amenity.objects.get(pk=pk)
+        except Amenity.DoesNotExist:
+            raise NotFound
 
-    def put(self, requset, pk):
-        pass
+    def get(self, request, pk):
+        amenitry = self.get_object(pk)
+        serializer = AmenitySerializer(amenitry)
 
-    def delete(self, requset, pk):
-        pass
+        return Response(
+            serializer.data,
+        )
+
+    def put(self, request, pk):
+        amenitry = self.get_object(pk)
+        serializer = AmenitySerializer(
+            amenitry,
+            data=request.data,
+            partial=True,
+        )
+
+        if serializer.is_valid():
+            update_amenity = serializer.save()
+            return Response(
+                AmenitySerializer(update_amenity).data,
+            )
+        else:
+            return Response(serial.errors)
+
+    def delete(self, request, pk):
+        amenitry = self.get_object(pk)
+        amenitry.delete()
+        return Response(status=HTTP_204_NO_CONTENT)
 
 
 # Create your views here.
