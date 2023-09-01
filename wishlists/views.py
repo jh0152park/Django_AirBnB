de8d995ec98f64f1419c9699eee468b7409d5747
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-09-01 23:35:35 +0900

    wishlists serializer

--- wishlists/views.py
+++ wishlists/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.exceptions import NotFound
+from rest_framework.exceptions import PermissionDenied
+from rest_framework.status import HTTP_200_OK
+from rest_framework.status import HTTP_404_NOT_FOUND
 
-# Create your views here.
+from .models import Wishlist
+from .serializers import WishlistSerializer
+
+
+class Wishlists(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        all_wishlists = Wishlist.objects.all().filter(user=request.user)
+        serializer = WishlistSerializer(
+            all_wishlists,
+            many=True,
+            context={"request": request},
+        )
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = WishlistSerializer(data=request.data)
+        if serializer.is_valid():
+            wishlist = serializer.save(user=request.user)
+            serializer = WishlistSerializer(wishlist)
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
