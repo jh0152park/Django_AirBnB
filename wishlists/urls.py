4b66253125e478a43183831acea30186a3889285
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-09-02 23:50:56 +0900

    created wishlist detail

--- wishlists/urls.py
+++ wishlists/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import Wishlists
+from .views import WishlistDetail
 
 urlpatterns = [
     path("", Wishlists.as_view()),
+    path("<int:pk>", WishlistDetail.as_view()),
 ]
