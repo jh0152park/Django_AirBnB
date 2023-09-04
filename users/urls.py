e9719a5d21b0dc843dbb45df018f26893049575c
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-09-04 23:33:06 +0900

    added login and logout

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 
 from .views import Me
+from .views import LogIn
+from .views import LogOut
 from .views import Users
 from .views import PublicUser
 from .views import changePassword
@@ -9,5 +11,7 @@ urlpatterns = [
     path("", Users.as_view()),
     path("me", Me.as_view()),
     path("change-password", changePassword.as_view()),
+    path("log-in", LogIn.as_view()),
+    path("log-out", LogOut.as_view()),
     path("@<str:username>", PublicUser.as_view()),
 ]
