e07fcddfe589b9d4f29aa1e168c4e8a457c67c57
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-09-08 00:54:20 +0900

    create env file to hidnig secret source

--- config/settings.py
+++ config/settings.py
@@ -11,16 +11,23 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import environ
+
+env = environ.Env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-k-iw%tdgihj($qt)x#(te1%0*%13yx9bwimrxw*d^517+20aa0"
+SECRET_KEY = env("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
