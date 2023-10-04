e7995ea0e4bb18f141419c37f18d4b2f1322207c
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-10-04 23:12:01 +0900

    refactorying

--- config/schema.py
+++ config/schema.py
@@ -16,30 +16,53 @@ movies_db = [
 ]
 
 
+def movies():
+    return movies_db
+
+
+def movie(movie_pk: int):
+    return movies_db[movie_pk - 1]
+
+
 @strawberry.type
 class Query:
-    @strawberry.field
-    def movies(self) -> typing.List[Movie]:
-        return movies_db
+    # @strawberry.field
+    # def movies(self) -> typing.List[Movie]:
+    #     return movies_db
 
-    @strawberry.field
-    def movie(self, movie_pk: int) -> Movie:
-        return movies_db[movie_pk - 1]
+    # @strawberry.field
+    # def movie(self, movie_pk: int) -> Movie:
+    #     return movies_db[movie_pk - 1]
+    movies: typing.List[Movie] = strawberry.field(resolver=movies)
+    movie: Movie = strawberry.field(resolver=movie)
+
+
+def add_movie(title: str, year: int, rating: int):
+    movies_db.append(
+        Movie(
+            pk=len(movies_db) + 1,
+            title=title,
+            year=year,
+            rating=rating,
+        )
+    )
+    return movies_db[-1]
 
 
 @strawberry.type
 class Mutation:
-    @strawberry.mutation
-    def add_movie(self, title: str, year: int, rating: int) -> Movie:
-        movies_db.append(
-            Movie(
-                pk=len(movies_db) + 1,
-                title=title,
-                year=year,
-                rating=rating,
-            )
-        )
-        return movies_db[-1]
+    # @strawberry.mutation
+    # def add_movie(self, title: str, year: int, rating: int) -> Movie:
+    #     movies_db.append(
+    #         Movie(
+    #             pk=len(movies_db) + 1,
+    #             title=title,
+    #             year=year,
+    #             rating=rating,
+    #         )
+    #     )
+    #     return movies_db[-1]
+    add_movie: Movie = strawberry.mutation(resolver=add_movie)
 
 
 schema = strawberry.Schema(
